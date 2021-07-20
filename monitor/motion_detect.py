4b72a12494448c71f2b876bea3b37fba1b8d1e29
prokaryote-hgy <1304905782@qq.com>
2021-07-20 09:34:28 +0800

    update

--- monitor/motion_detect.py
+++ monitor/motion_detect.py
@@ -11,7 +11,7 @@ save_path = './face/'
 face_cascade = cv2.CascadeClassifier(pathf)
 eye_cascade = cv2.CascadeClassifier(pathe)
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5x, custom
+# model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5m, yolov5x, custom
 
 # camera = cv2.VideoCapture(0) # 参数0表示第一个摄像头
 # cv2.namedWindow("camera",1)
@@ -38,9 +38,9 @@ i = 0
 flag = None
 cnt = 0
 while True:
-    if cnt % 3 != 0:
-        cnt += 1
-        continue
+    # if cnt % 3 != 0:
+    #     cnt += 1
+    #     continue
     cnt = 0
     start = time.time()
     grabbed, frame_lwpCV = camera.read() # 读取视频流
@@ -86,20 +86,21 @@ while True:
             # text = "test"
             # ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S %f')
             # cv2.putText(frame_lwpCV, ts, (10, frame_lwpCV.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-            # yolov5检测物体
-            pre_frame = gray_lwpCV
-            # Images
-            img = frame_lwpCV  # or file, PIL, OpenCV, numpy, multiple
-            # Inference
-            results = model(img)
-            # Results
-            # results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
-            # print(type(results.pandas().xyxy[0]['xmin'].values[0]))
-            if len(results.pandas().xyxy[0]['xmin'].values) != 0:
-                cv2.rectangle(frame_lwpCV, (
-                    int(results.pandas().xyxy[0]['xmin'].values[0]), int(results.pandas().xyxy[0]['ymin'].values[0])),
-                    (int(results.pandas().xyxy[0]['xmax'].values[0]), int(results.pandas().xyxy[0]['ymax'].values[0])),
-                        (255, 255, 0), 2)
+
+            # # yolov5检测物体
+            # pre_frame = gray_lwpCV
+            # # Images
+            # img = frame_lwpCV  # or file, PIL, OpenCV, numpy, multiple
+            # # Inference
+            # results = model(img)
+            # # Results
+            # # results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
+            # # print(type(results.pandas().xyxy[0]['xmin'].values[0]))
+            # if len(results.pandas().xyxy[0]['xmin'].values) != 0:
+            #     cv2.rectangle(frame_lwpCV, (
+            #         int(results.pandas().xyxy[0]['xmin'].values[0]), int(results.pandas().xyxy[0]['ymin'].values[0])),
+            #         (int(results.pandas().xyxy[0]['xmax'].values[0]), int(results.pandas().xyxy[0]['ymax'].values[0])),
+            #             (255, 255, 0), 2)
 
         cv2.imshow('lwpCVWindow', frame_lwpCV)
 
