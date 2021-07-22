db649a462cf66e570ef8937c99a3543696bde6c1
kambition <67893995+kambition@users.noreply.github.com>
2021-07-22 14:53:08 +0800

    Create registerFirst.js

---
+++ weChatView-1.0/registerFirst/registerFirst.js
@@ -0,0 +1,17 @@
+// pages/registerFirst/registerFirst.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  jumpPage:function(){
+    wx.navigateTo({
+      url: '/pages/register/register',
+    })
+  }
+
+})
