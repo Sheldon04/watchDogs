7d1b85bee911f3074e6d0f764f347f104c90a1fe
prokaryote-hgy <1304905782@qq.com>
2021-07-22 20:56:10 +0800

    update

--- users/src/components/user/TracebackUser.vue
+++ users/src/components/user/TracebackUser.vue
@@ -54,6 +54,26 @@
               </td>
             </tr>
           </table>
+          <el-calendar :first-day-of-week=7>
+            <template
+              slot="dateCell"
+              slot-scope="{date, data}">
+              <p>
+                {{ data.day.split('-').slice(1).join('-') }}
+<!--                v-if="data.day==='2021-07-22'||data.day=='2021-07-23'"-->
+                <el-tag v-if="parseInt(data.day.split('-')[1]) === cur_month && month_invasion_data[data.day.split('-')[2]] > 0 && month_invasion_data[data.day.split('-')[2]] <= 5" type="warning">
+                  {{month_invasion_data[data.day.split('-')[2]]}}
+                </el-tag>
+                <el-tag v-if="parseInt(data.day.split('-')[1]) === cur_month && month_invasion_data[data.day.split('-')[2]] > 5" type="danger">
+                  {{month_invasion_data[data.day.split('-')[2]]}}
+                </el-tag>
+              </p>
+              <!--标记-->
+<!--              <div v-if="data.day==='2021-07-22'||data.day=='2021-07-23'" class="red budge">5</div>-->
+<!--              <div v-if="data.day==='2021-07-12'||data.day=='2021-07-13'" class="green budge"></div>-->
+<!--              <div v-if="data.day==='2021-07-02'||data.day=='2021-07-03'" class="orange budge"></div>-->
+            </template>
+          </el-calendar>
         </el-main>
       </el-container>
     </div>
@@ -68,14 +88,45 @@ export default {
       activeIndex: this.$route.path,
       imgSrc: require('../../assets/img3.jpg'),
       options: [],
-      value: ''
+      value: '',
+      cur_month: 0,
+      month_invasion_data: {
+        '12': 2,
+        '23': 5,
+        '24': 7,
+        '27': 9
+      },
+      testdata: 5
     }
   },
   methods: {
     handleSelect (key, keyPath) {
       console.log(key, keyPath)
       this.$router.push(key)
+    },
+    set_cur_month () {
+      let nowDate = new Date()
+      this.cur_month = nowDate.getMonth() + 1
     }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      // 点击前一个月
+      let prevBtn = document.querySelector(
+        '.el-calendar__button-group .el-button-group>button:nth-child(1)')
+      prevBtn.addEventListener('click', () => {
+        console.log(this.cur_month)
+      })
+    })
+    this.$nextTick(() => {
+      // 点击后一个月
+      let prevBtn = document.querySelector(
+        '.el-calendar__button-group .el-button-group>button:last-child')
+      prevBtn.addEventListener('click', () => {
+        console.log(this.cur_month)
+      })
+    })
+    this.set_cur_month()
   }
 }
 </script>
@@ -93,6 +144,23 @@ export default {
   left: 200px;
   top: 80px;
   position: absolute;
+}
+
+.budge{
+  width: 10px;
+  height: 10px;
+  border-radius: 5px;
+  margin: 5px auto;
+}
+
+.red{
+  background-color: #c9413f;
+}
+.green{
+  background-color: #25b591;
+}
+.orange{
+  background-color: #ee915c;
 }
 
 </style>
